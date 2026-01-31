484d97468dd6644d56d8acd54ee8e96332d61a3e
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-01-31 21:50:36 +0600

    chore: set up base project architecture

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //container colors
+  static const Color btnBackgroundColor = Color(0xff2A4566);
+
+  static const Color secondaryColor = Color(0xff6C3BFF);
+  static const Color primaryColor = Color(0xff3A7AFE);
+
+}
---
+++ lib/core/network/api_endpoints.dart
@@ -0,0 +1,5 @@
+class ApiEndpoints {
+  static const String baseUrl =
+      "https://maui-intermediate-represented-option.trycloudflare.com";
+  static const String register = 'api/auth/register';
+}
