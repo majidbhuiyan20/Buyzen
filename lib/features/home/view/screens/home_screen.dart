ad792677abbc96375f20a2e5c3bd67d6af1fdfe9
Majid Bhuiyan <majid.bhuiyan2011@gmail.com>
2026-02-07 13:04:11 +0600

    feat(ui): recommendation ui complete

--- lib/features/home/view/screens/home_screen.dart
+++ lib/features/home/view/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:buyzen/app/app_text_style.dart';
 import 'package:buyzen/features/home/view/widgets/home_carousel_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -5,6 +6,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../app/app_colors.dart';
 import '../widgets/category_card.dart';
 import '../widgets/circle_icon_button.dart';
+import '../widgets/product_card_widgets.dart';
 import '../widgets/section_header.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -20,40 +22,66 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: _buildAppBar(),
-      body: Column(
-
-        children: [
-          SizedBox(height: 16.h,),
-          HomeCarouselWidgets(),
-          SizedBox(height: 1.h,),
-          SectionHeader(title: 'Categories', onTap: () {  },),
-          SingleChildScrollView(
-            scrollDirection: .horizontal,
-            child: SizedBox(
-              height: 68.h,
-              child: Padding(
-                padding: EdgeInsets.only(left: 16.0),
-                child: Row(
-                  children: [
-                    CategoryCard(imagePath:"assets/icons/nike.png", onTap: () {  },),
-                    SizedBox(width: 16.w,),
-                    CategoryCard(imagePath:"assets/icons/puma.png", onTap: () {  },),
-                    SizedBox(width: 16.w,),
-                    CategoryCard(imagePath:"assets/icons/adidas.png", onTap: () {  },),
-                    SizedBox(width: 16.w,),
-                    CategoryCard(imagePath:"assets/icons/jordan.png", onTap: () {  },),
-                    SizedBox(width: 16.w,),
-                    CategoryCard(imagePath:"assets/icons/lining.png", onTap: () {  },),
-                    SizedBox(width: 16.w,),
-                    CategoryCard(imagePath:"assets/icons/jordan.png", onTap: () {  },),
-                  ],
+      body: SingleChildScrollView(
+        child: Column(
+        
+          children: [
+            SizedBox(height: 16.h,),
+            HomeCarouselWidgets(),
+            SizedBox(height: 1.h,),
+            SectionHeader(title: 'Categories', onTap: () {  },),
+            SingleChildScrollView(
+              scrollDirection: .horizontal,
+              child: SizedBox(
+                height: 68.h,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 16.0),
+                  child: Row(
+                    children: [
+                      CategoryCard(imagePath:"assets/icons/nike.png", onTap: () {  },),
+                      SizedBox(width: 16.w,),
+                      CategoryCard(imagePath:"assets/icons/puma.png", onTap: () {  },),
+                      SizedBox(width: 16.w,),
+                      CategoryCard(imagePath:"assets/icons/adidas.png", onTap: () {  },),
+                      SizedBox(width: 16.w,),
+                      CategoryCard(imagePath:"assets/icons/jordan.png", onTap: () {  },),
+                      SizedBox(width: 16.w,),
+                      CategoryCard(imagePath:"assets/icons/lining.png", onTap: () {  },),
+                      SizedBox(width: 16.w,),
+                      CategoryCard(imagePath:"assets/icons/jordan.png", onTap: () {  },),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          SectionHeader(title: 'Recommendation', onTap: () {  },),
-
-        ],
+            SectionHeader(title: 'Recommendation', onTap: () {  },),
+            Padding(
+              padding:  EdgeInsets.symmetric(horizontal: 16.w),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: .spaceBetween,
+                    children: [
+                      ProductCardWidgets(),
+                      SizedBox(width: 12.w,),
+                      ProductCardWidgets(),
+                    ],
+                  ),
+                  SizedBox(height: 16.h,),
+                  Row(
+                    mainAxisAlignment: .spaceBetween,
+                    children: [
+                      ProductCardWidgets(),
+                      SizedBox(width: 12.w,),
+                      ProductCardWidgets(),
+                    ],
+                  ),
+                ],
+              ),
+            )
+        
+          ],
+        ),
       ),
     );
   }
@@ -83,6 +111,8 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
 
 
 
---
+++ lib/features/home/view/widgets/product_card_widgets.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../app/app_colors.dart';
+class ProductCardWidgets extends StatelessWidget {
+  const ProductCardWidgets({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 165.w,
+          padding: EdgeInsets.all(16.r),
+          decoration: BoxDecoration(
+            color: Color(0XFF562CB5).withAlpha(20),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(8.r),
+              topRight: Radius.circular(8.r),
+            ),
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.favorite_outline_outlined),
+                  Spacer(),
+                  Text(""),
+                ],
+              ),
+
+              Image.asset("assets/images/shoe4.png", height: 80,),
+            ],
+          ),
+        ),
+        Container(
+          width: 150.w,
+          padding: EdgeInsets.all(16.r),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            // boxShadow: [
+            //   BoxShadow(
+            //     color: Colors.black.withOpacity(0.15),
+            //     offset: Offset(0, 4),
+            //     blurRadius: 6,
+            //     spreadRadius: 0,
+            //   ),
+            // ],
+          ),
+          child: Column(
+            crossAxisAlignment: .start,
+            children: [
+              Row(
+                children: [
+                  Text("\$129.00", style: TextStyle(fontWeight: .w700, fontSize: 16.sp, color: AppColors.themeColor),),
+                  Spacer(),
+                  Wrap(
+                    children: [
+                      Icon(Icons.star, color: Colors.amber, size: 18.sp,),
+                      SizedBox(width: 4.w,),
+                      Text("4.9", style: TextStyle(fontWeight: .w600, fontSize: 14.sp),),
+
+                    ],
+                  )
+                ],
+              ),
+              Text("Nike Lebron 8", style: TextStyle(fontWeight: .w500, fontSize: 15.sp), overflow: .ellipsis,),
+
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
